ec56824f303bb19cb738bb4aba01acdcb5aeb699
jcvong <vongcjohnny@gmail.com>
2020-01-25 20:36:39 -0800

    Basic arm movement.

--- ICS167 Project 1/Assets/Scripts/LeftArmMovement.cs
+++ ICS167 Project 1/Assets/Scripts/LeftArmMovement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class LeftArmMovement : MonoBehaviour
 {
+    [SerializeField] public GameObject leftArmTarget;
+
+    private Transform leftTransform;
+    private int speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftTransform = leftArmTarget.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontalModifier = Input.GetAxisRaw("Horizontal");
+        float verticalModifier = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(horizontalModifier);
+
+        leftTransform.Translate(horizontalModifier * (Time.deltaTime * 20), verticalModifier * (Time.deltaTime * 20), 0);
     }
 }
