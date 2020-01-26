f8aabd82473fbca598feae5ceb9798e9396ba3e8
Michelle Wang <zhiyimichelle@gmail.com>
2020-01-25 18:59:06 -0800

    added billboarding and transparent BG

---
+++ ICS167 Project 1/Assets/Scripts/BillBoarding.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoarding : MonoBehaviour
+{
+
+    [SerializeField] bool isBillBoard = false;
+    private Camera m_Camera;
+ 
+
+    private void Awake()
+    {
+        m_Camera = Camera.main;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        if (isBillBoard)
+        {
+            Vector3 temp = new Vector3(m_Camera.transform.position.x, -90, m_Camera.transform.position.z);
+            transform.LookAt(temp);
+        }
+
+    }
+}
