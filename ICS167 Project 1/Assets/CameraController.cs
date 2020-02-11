1d8def39706a42c7fe1e132aae16295311a6ac4a
Michaela Gonzales <michacg2@uci.edu>
2020-02-11 00:10:49 -0800

    camera and buttons all follow plaen movement :) but debugging arms. button functionality merging next

---
+++ ICS167 Project 1/Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
