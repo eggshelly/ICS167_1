566016acba25f18ff3b8acd61d8f27cdf43e3f46
Daniel Qi <drqi@gmail.com>
2020-02-11 18:48:15 -0800

    ok bounds work

--- ICS167 Project 1/Assets/PlaneConstraints.cs
+++ ICS167 Project 1/Assets/PlaneConstraints.cs
@@ -33,9 +33,15 @@ public class PlaneConstraints : MonoBehaviour
         Vector3 left = plane.transform.TransformPoint(new Vector3(center.x - c.size.x / 2, center.y, center.z + c.size.y));
         Vector3 middle = plane.transform.TransformPoint(new Vector3(center.x, center.y, center.z + c.size.y)); 
         Vector3 right = plane.transform.TransformPoint(new Vector3(center.x + c.size.x / 2, center.y, center.z + c.size.y));
-        
-        
-        if(left.x < xmin || middle.x < xmin || right.x < xmin)
+
+        Vector3 bleft = plane.transform.TransformPoint(new Vector3(center.x - c.size.x / 2, center.y, center.z - c.size.y));
+        Vector3 bmiddle = plane.transform.TransformPoint(new Vector3(center.x, center.y, center.z - c.size.y));
+        Vector3 bright = plane.transform.TransformPoint(new Vector3(center.x + c.size.x / 2, center.y, center.z - c.size.y));
+
+
+
+
+        if (left.x < xmin || middle.x < xmin || right.x < xmin)
         {
             plane.transform.position += Vector3.right * (Mathf.Max(xmin - left.x, Mathf.Max(xmin - middle.x, xmin - right.x)) + shift);
             rb.velocity = Vector3.zero;
@@ -58,7 +64,31 @@ public class PlaneConstraints : MonoBehaviour
             plane.transform.position += Vector3.back * (Mathf.Max(zmax - left.z, Mathf.Max(zmax - middle.z, zmax - right.z)) +  shift);
             rb.velocity = Vector3.zero;
         }
-   
+
+
+        if (bleft.x < xmin || bmiddle.x < xmin || bright.x < xmin)
+        {
+            plane.transform.position += Vector3.right * (Mathf.Max(xmin - bleft.x, Mathf.Max(xmin - bmiddle.x, xmin - bright.x)) + shift);
+            rb.velocity = Vector3.zero;
+        }
+        else if (bleft.x > xmax || bmiddle.x > xmax || bright.x > xmax)
+        {
+            plane.transform.position += Vector3.left * (Mathf.Max(xmax - bleft.x, Mathf.Max(xmax - bmiddle.x, xmax - bright.x)) + shift);
+            rb.velocity = Vector3.zero;
+        }
+
+
+        if (bleft.z < zmin || bmiddle.z < zmin || bright.z < zmin)
+        {
+            plane.transform.position += Vector3.forward * (Mathf.Max(zmin - bleft.z, Mathf.Max(zmin - bmiddle.z, zmin - bright.z)) + shift);
+            rb.velocity = Vector3.zero;
+        }
+
+        else if (bleft.z > zmax || bmiddle.z > zmax || bright.z > zmax)
+        {
+            plane.transform.position += Vector3.back * (Mathf.Max(zmax - bleft.z, Mathf.Max(zmax - bmiddle.z, zmax - bright.z)) + shift);
+            rb.velocity = Vector3.zero;
+        }
 
     }
 
