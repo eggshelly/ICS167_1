6a8d01d875f7498bd0ce9a159abe3ad73d2fb763
Daniel Qi <drqi@gmail.com>
2020-02-11 18:43:45 -0800

    setting velocity to 0 kinda fixes it

--- ICS167 Project 1/Assets/PlaneConstraints.cs
+++ ICS167 Project 1/Assets/PlaneConstraints.cs
@@ -38,21 +38,25 @@ public class PlaneConstraints : MonoBehaviour
         if(left.x < xmin || middle.x < xmin || right.x < xmin)
         {
             plane.transform.position += Vector3.right * (Mathf.Max(xmin - left.x, Mathf.Max(xmin - middle.x, xmin - right.x)) + shift);
+            rb.velocity = Vector3.zero;
         }
         else if(left.x > xmax || middle.x > xmax || right.x > xmax)
         {
             plane.transform.position += Vector3.left * ( Mathf.Max(xmax - left.x, Mathf.Max(xmax - middle.x, xmax - right.x)) + shift);
+            rb.velocity = Vector3.zero;
         }
 
 
         if (left.z < zmin || middle.z < zmin || right.z < zmin)
         {
             plane.transform.position += Vector3.forward * (Mathf.Max(zmin - left.z, Mathf.Max(zmin - middle.z, zmin - right.z)) + shift);
+            rb.velocity = Vector3.zero;
         }
     
         else if (left.z > zmax || middle.z > zmax || right.z > zmax)
         {
             plane.transform.position += Vector3.back * (Mathf.Max(zmax - left.z, Mathf.Max(zmax - middle.z, zmax - right.z)) +  shift);
+            rb.velocity = Vector3.zero;
         }
    
 
